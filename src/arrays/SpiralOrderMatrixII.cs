2e9964f9b629648bd0c64a7f0ef095b462a50606
Konstantin Suslov <suslov93@gmail.com>
2021-01-14 09:37:24 +0300

    spiral order matrix ii

---
+++ src/arrays/SpiralOrderMatrixII.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace arrays
+{
+    // ReSharper disable once InconsistentNaming
+    public class SpiralOrderMatrixII
+    {
+        public static List<List<int>> Generate(int dimensions)
+        {
+            var result = new int[dimensions][];
+            for (var i = 0; i < dimensions; i++)
+                result[i] = new int[dimensions];
+
+            int number = 1,
+                left = 0,
+                right = dimensions - 1,
+                top = 0,
+                down = dimensions - 1,
+                // 0 - right, 1 - down, 2 - left, 3 - up
+                direction = 0;
+
+            while (number <= dimensions * dimensions)
+            {
+                switch (direction % 4)
+                {
+                    case 0:
+                        for (var i = left; i <= right; i++)
+                            result[top][i] = number++;
+                        top++;
+                        break;
+                    case 1:
+                        for (var i = top; i <= down; i++)
+                            result[i][right] = number++;
+                        right--;
+                        break;
+                    case 2:
+                        for (var i = right; i >= left; i--)
+                            result[down][i] = number++;
+                        down--;
+                        break;
+                    case 3:
+                        for (var i = down; i >= top; i--)
+                            result[i][left] = number++;
+                        left++;
+                        break;
+                }
+
+                direction++;
+            }
+            
+            return result.Select(r => r.ToList()).ToList();
+        }
+    }
+}
---
+++ tests/arrays.tests/SpiralOrderMatrixIITests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace arrays
+{
+    // ReSharper disable once InconsistentNaming
+    public class SpiralOrderMatrixIITests
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "1,2;4,3")]
+        [InlineData(3, "1,2,3;8,9,4;7,6,5")]
+        [InlineData(4, "1,2,3,4;12,13,14,5;11,16,15,6;10,9,8,7")]
+        [InlineData(5, "1,2,3,4,5;16,17,18,19,6;15,24,25,20,7;14,23,22,21,8;13,12,11,10,9")]
+        public void Generate_ShouldReturnValidMatrix(int dimensions, string expected)
+        {
+            var result = SpiralOrderMatrixII.Generate(dimensions);
+            var actual = string.Join(';', result.Select(row => string.Join(',', row)));
+            Assert.Equal(expected, actual);
+        }
+    }
+}
